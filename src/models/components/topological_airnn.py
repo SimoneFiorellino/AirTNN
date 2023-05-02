963ddb8830785279c93457e4875c66fceb5b0e55
SimoneFiorellino <simone.fiorellino@uniroma1.it>
2023-05-02 15:00:18 +0200

    topological and trainer opt

--- src/models/components/topological_airnn.py
+++ src/models/components/topological_airnn.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
+from torch.nn import Linear
 
 class TAirGNN(nn.Module):
 
@@ -8,16 +9,15 @@ class TAirGNN(nn.Module):
         """function to add white noise to the input signal"""
         with torch.no_grad():
             z_clone = torch.detach(z)
-            snr_lin = 10 ** (self.snr_db / 10)  # SNRdb to linear
             x_power = torch.sum(z_clone ** 2) / torch.numel(z_clone)
-            var = x_power / snr_lin
+            var = x_power / self.snr_lin
             std = torch.sqrt(var)
             return torch.randn_like(z_clone) * std
-        
+
     def _channel_fading(self, adj):
         """function to generate channel fading"""
         with torch.no_grad():
-            s_air = torch.randn_like(adj, dtype=torch.complex64)
+            s_air = torch.randn_like(adj, dtype=torch.complex64) * torch.sqrt(torch.tensor(0.5, dtype=torch.float32))
             s_air = torch.abs(s_air)
             return s_air
 
@@ -26,14 +26,23 @@ class TAirGNN(nn.Module):
         self.c_in = c_in
         self.c_out = c_out
         self.snr_db = snr_db
+        self.snr_lin = 10 ** (self.snr_db / 10)  # SNRdb to linear        
         self.k = k
-        self.up_weight = nn.Parameter(torch.Tensor(c_in, c_out, k))
-        self.low_weight = nn.Parameter(torch.Tensor(c_in, c_out, k))
+        # self.up_weight = nn.Parameter(torch.Tensor(c_in, c_out, k))
+        # self.low_weight = nn.Parameter(torch.Tensor(c_in, c_out, k))
+        self.up_lins = torch.nn.ModuleList([
+            Linear(c_in, c_out, bias=False) for _ in range(k + 1)
+        ])
+        self.low_lins = torch.nn.ModuleList([
+            Linear(c_in, c_out, bias=False) for _ in range(k + 1)
+        ])
+        self.reset_parameters()
 
     def reset_parameters(self):
-        """function to initialize the weight matrix"""
-        nn.init.xavier_uniform_(self.up_weight)
-        nn.init.xavier_uniform_(self.low_weight)
+        for lin in self.up_lins:
+            lin.reset_parameters()
+        for lin in self.low_lins:
+            lin.reset_parameters()
 
     def shift(self, x_up, x_low, upper_lp, lower_lp):
         """function to shift the input signal:
@@ -42,9 +51,11 @@ class TAirGNN(nn.Module):
         3. add white noise"""
         fading = self._channel_fading(upper_lp)
         noise  = self._white_noise(x_up)
-        x_up  = torch.einsum("ij,jk->ik", (upper_lp * fading, x_up )) + noise # (S_up @ x_up + n)
-        x_low = torch.einsum("ij,jk->ik", (lower_lp * fading, x_low)) + noise # (S_low @ x_low + n)
+        x_up  = torch.bmm(upper_lp * fading, x_up ) + noise # (S_up @ x_up + n)
+        x_low = torch.bmm(lower_lp * fading, x_low) + noise # (S_low @ x_low + n)
         return x_up, x_low
+    
+
 
     def forward(self, x, upper_lp, lower_lp):
         """function to forward the input signal:
@@ -53,20 +64,20 @@ class TAirGNN(nn.Module):
         3. sum the output of each shift"""
 
         x_up, x_low = self.shift(x, x, upper_lp, lower_lp)
-        out_up  = torch.einsum("ij,jk->ik", (x_up, self.up_weight[:,:,0]))
-        out_low = torch.einsum("ij,jk->ik", (x_low, self.low_weight[:,:,0]))
+        out_up  = self.up_lins[0].forward(x)
+        out_low = self.low_lins[0].forward(x)
 
-        for i in range(1, self.k):
+        for up_lin, low_lin in zip(self.up_lins[1:], self.low_lins[1:]):
             x_up, x_low = self.shift(x_up, x_low, upper_lp, lower_lp)
-            out_up += torch.einsum("ij,jk->ik", (x_up, self.up_weight[:,:,i]))
-            out_low += torch.einsum("ij,jk->ik", (x_low, self.low_weight[:,:,i]))
+            out_up = out_up + up_lin.forward(x_up)
+            out_low = out_low + low_lin.forward(x_low)
 
         return out_up + out_low
 
 if __name__ == '__main__':
     # test
-    x = torch.randn(10, 3)
-    upper_lp = torch.randn(10, 10)
-    lower_lp = torch.randn(10, 10)
+    x = torch.randn(64, 10, 3)
+    upper_lp = torch.randn(64, 10, 10)
+    lower_lp = torch.randn(64, 10, 10)
     model = TAirGNN(3, 4, k = 10)
     out = model(x, upper_lp, lower_lp)
