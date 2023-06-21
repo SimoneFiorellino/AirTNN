66bd4b984c3a98733d1f4a2336e82159cdd470ae
SimoneFiorellino <simone.fiorellino@uniroma1.it>
2023-06-21 18:32:38 +0200

    just a little bit of order

--- plots/snr_plots.py
+++ plots/snr_plots.py
@@ -1,83 +1,31 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# # Load the CSV file
-# df = pd.read_csv('plots/snrdb_acc.csv')
-
-# # Filter DataFrame based on 'model' and group by 'snr_db'
-# grouped1 = df[df['model'] == 'airtnn'].groupby('model.backbone.snr_db')['test/acc'].agg(['mean', 'std'])
-# grouped2 = df[df['model'] == 'airgnn'].groupby('model.backbone.snr_db')['test/acc'].agg(['mean', 'std'])
-
-# # Plot mean with standard deviation bars for both datasets
-# plt.errorbar(grouped1.index, grouped1['mean'], yerr=grouped1['std'], 
-#              fmt='o', label='AirTNN Mean with STD bars', color='blue')
-# plt.errorbar(grouped2.index, grouped2['mean'], yerr=grouped2['std'], 
-#              fmt='o', label='AirGNN Mean with STD bars', color='green')
-
-# # Plot line connecting the means for both datasets
-# plt.plot(grouped1.index, grouped1['mean'], label='AirTNN', color='red')
-# plt.plot(grouped2.index, grouped2['mean'], label='AirGNN', color='orange')
-
-# # Add labels
-# plt.xlabel('snr_db')
-# plt.ylabel('accuracy')
-# plt.title('Mean and STD of Accuracy Grouped by snr_db')
-
-# # Add legend
-# plt.legend()
-
-# # Add grid
-# plt.grid(True)
-
-# plt.savefig('plots/snr_db_comparison_plot.png', dpi=300)
-
-# # Show the plot
-# plt.show()
-
 import pandas as pd
 import matplotlib.pyplot as plt
-import itertools
 
 # Load the CSV file
 df = pd.read_csv('plots/snrdb_acc.csv')
 
-# Get unique models
-models = df['model'].unique()
+# Get the unique models and number of parameters in the DataFrame
+unique_models = df['model'].unique()
+unique_num_params = df['num_params'].unique()
 
-# Create a list of colors
-colors = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
+# Create a list of colors for the models. Adjust as needed.
+colors = ['blue', 'green', 'red', 'orange', 'purple', 'brown']
 
-# Placeholder for legend elements
-legend_elements = []
+# Create a plot for each model
+index = 0
+for model in unique_models:
+    for num_param in unique_num_params:
+        sub_df = df[(df['model'] == model) & (df['num_params'] == num_param)]
+        if not sub_df.empty:
+            grouped = sub_df.groupby('model/backbone/snr_db')['test/acc'].agg(['mean', 'std'])
+            model_with_params = model + " " + str(num_param // 10**3) + 'K' + " params"
+            plt.errorbar(grouped.index, grouped['mean'], yerr=grouped['std'], 
+                         fmt='o', color=colors[index % len(colors)])
+            plt.plot(grouped.index, grouped['mean'], label=model_with_params, color=colors[index % len(colors)])
+            index += 1
 
-for model in models:
-    # Filter by model type
-    df_model = df[df['model'] == model]
-    
-    # Get unique combinations of hyperparameters for each model
-    hyperparameters = df_model[['model/backbone/k', 'model/backbone/hidden_dim']].drop_duplicates()
-    
-    for idx, row in hyperparameters.iterrows():
-        # Get next color
-        color = next(colors)
-        
-        # Filter DataFrame based on hyperparameters
-        df_hyper = df_model[(df_model['model/backbone/k'] == row['model/backbone/k']) & 
-                            (df_model['model/backbone/hidden_dim'] == row['model/backbone/hidden_dim'])]
-        
-        # Group by 'snr_db' and compute mean and std
-        grouped = df_hyper.groupby('model.backbone.snr_db')['test/acc'].agg(['mean', 'std'])
-        
-        # Plot mean with standard deviation bars
-        plt.errorbar(grouped.index, grouped['mean'], yerr=grouped['std'], 
-                     fmt='o', label=f'{model} {row["model/backbone/k"]}/{row["model/backbone/hidden_dim"]} Mean with STD bars', 
-                     color=color)
-        
-        # Plot line connecting the means
-        plt.plot(grouped.index, grouped['mean'], label=f'{model} {row["model/backbone/k"]}/{row["model/backbone/hidden_dim"]}', 
-                 color=color)
-        
-        legend_elements.append(f'{model} {row["model/backbone/k"]}/{row["model/backbone/hidden_dim"]}')
+plt.axhline(y=0.97, color='r', linestyle='--', label='Models without fading and noise')
+
 
 # Add labels
 plt.xlabel('snr_db')
@@ -85,7 +33,7 @@ plt.ylabel('accuracy')
 plt.title('Mean and STD of Accuracy Grouped by snr_db')
 
 # Add legend
-plt.legend(legend_elements, loc='lower right')
+plt.legend()
 
 # Add grid
 plt.grid(True)
@@ -94,4 +42,3 @@ plt.savefig('plots/snr_db_comparison_plot.png', dpi=300)
 
 # Show the plot
 plt.show()
-
